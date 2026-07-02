4de08a92d68930a5764ae3a5bbf53304b8a8ef70
Roy <Rakesh@Apples-MacBook-Pro.local>
2026-07-03 03:34:40 +0530

    configure terraform s3 backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "your-unique-tf-state-bucket"
-    key            = "nginx/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket = "tf-state-482178843185-nginx"
+    key    = "nginx/terraform.tfstate"
+    region = "ap-south-1"
   }
 }
